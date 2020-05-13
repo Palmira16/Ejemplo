a3a2ab2cd247fa64e6278f493cab2a230fa88d9c
Usuario <Usuario@Usuario-PC>
2020-05-13 12:30:09 -0600

    primer commit del proyecto muestra saludo

---
+++ src/ejemplo_github/Ejemplo_GitHub.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_github;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejemplo_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenidos...!");
+        System.out.println("Welcome!");
+        System.out.println("Hi...!");
+    }
+    
+}
